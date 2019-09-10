55ee64aa977652cafbbf5664b137197a9bf72791
Simon Lervad <lervad1996@gmail.com>
2019-09-10 12:54:25 +0200

    sidste opgave

--- jsArrays1.js
+++ jsArrays1.js
@@ -7,6 +7,7 @@ function play(a) {
 let counter = [0, 0, 0, 0, 0, 0, 0]
 
 flip = play(6);
+
 if (flip === 1)
     counter[1]++;
 else if (flip === 2)
@@ -20,4 +21,108 @@ else if (flip === 5)
 else 
     counter[6]++;
 
+function play(a) {
+	let out = Math.random();
+	out = Math.floor(out*a+1);
+	return out;
+}
+
+flip = play(6);
+
+if (flip === 1)
+    counter[1]++;
+else if (flip === 2)
+    counter[2]++;
+else if (flip === 3)
+    counter[3]++;
+else if (flip === 4)
+    counter[4]++;
+else if (flip === 5)
+    counter[5]++;
+else 
+    counter[6]++;
+
+function play(a) {
+	let out = Math.random();
+	out = Math.floor(out*a+1);
+	return out;
+}
+
+flip = play(6);
+
+if (flip === 1)
+    counter[1]++;
+else if (flip === 2)
+    counter[2]++;
+else if (flip === 3)
+    counter[3]++;
+else if (flip === 4)
+    counter[4]++;
+else if (flip === 5)
+    counter[5]++;
+else 
+    counter[6]++;
+
+function play(a) {
+	let out = Math.random();
+	out = Math.floor(out*a+1);
+	return out;
+}
+
+flip = play(6);
+
+if (flip === 1)
+    counter[1]++;
+else if (flip === 2)
+    counter[2]++;
+else if (flip === 3)
+    counter[3]++;
+else if (flip === 4)
+    counter[4]++;
+else if (flip === 5)
+    counter[5]++;
+else 
+    counter[6]++;
+
+function play(a) {
+	let out = Math.random();
+	out = Math.floor(out*a+1);
+	return out;
+}
+
+flip = play(6);
+
+if (flip === 1)
+    counter[1]++;
+else if (flip === 2)
+    counter[2]++;
+else if (flip === 3)
+    counter[3]++;
+else if (flip === 4)
+    counter[4]++;
+else if (flip === 5)
+    counter[5]++;
+else 
+    counter[6]++;
+
+function play(a) {
+	let out = Math.random();
+	out = Math.floor(out*a+1);
+	return out;
+}
+
+flip = play(6);
+
+if (flip === 1)
+    counter[1]++;
+else if (flip === 2)
+    counter[2]++;
+else if (flip === 3)
+    counter[3]++;
+else if (flip === 4)
+    counter[4]++;
+else if (flip === 5)
+    counter[5]++;
+else 
+    counter[6]++;
 console.table(counter);
